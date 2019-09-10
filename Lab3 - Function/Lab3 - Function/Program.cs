bc9cd381d6231c92079d6cb97ed3d36a22b6d597
Solum96 <nils.solumfaeste@gmail.com>
2019-09-10 13:54:32 +0200

    Added switch/case menu and created new Method for FindRoute(); added TODO: in FindRoute()

--- Lab3 - Function/Lab3 - Function/Program.cs
+++ Lab3 - Function/Lab3 - Function/Program.cs
@@ -19,8 +19,61 @@ namespace Lab3___Function
         static string[] Places = new string[] { "Stockholm", "Göteborg", "Oslo", "Luleå", "Helsinki", "Berlin", "Paris" };
         static double[] Latitudes = new double[] { 59.3261917, 57.7010496, 59.8939529, 65.5867395, 60.11021, 52.5069312, 48.859 };
         static double[] Longitudes = new double[] { 17.7018773, 11.6136602, 10.6450348, 22.0422998, 24.7385057, 13.1445521, 2.2069765 };
+        static string[] Route;
 
         static void Main(string[] args)
+        {
+            bool loop = true;
+            while (loop == true)
+            {
+                try
+                {
+
+                    Console.WriteLine("Select a service:");
+                    Console.WriteLine("[1] Find distance between two cities");
+                    Console.WriteLine("[2] Find distance in route");
+                    Console.WriteLine("[0] EXIT");
+                    int menu = int.Parse(Console.ReadLine());   
+                    switch (menu)
+                    {
+                        case 1:
+                            Console.Clear();
+                            FindDistance();
+                            break;
+
+                        case 2:
+                            Console.Clear();
+                            FindRoute();
+                            break;
+
+                        case 0:
+                            loop = false;
+                            break;
+
+                        default:
+                            throw new Exception("Invalid command.");
+                    }
+                }
+                catch (ArgumentException e)
+                {
+                    Console.Clear();
+                    Console.WriteLine(e.Message);
+                }
+                catch (FormatException)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Use numbers.");
+                }
+            }
+            
+        }
+
+        private static void FindRoute()
+        {
+            //TODO: Write method for FindRoute.
+        }
+
+        private static void FindDistance()
         {
             while (true)
             {
@@ -35,9 +88,9 @@ namespace Lab3___Function
                     Console.WriteLine("[6] Berlin");
                     Console.WriteLine("[7] Paris");
 
-                    int menu = int.Parse(Console.ReadLine());
-                    SetStartLocation(menu);
-                    if (menu < 0 && menu > 7) throw new ArgumentException("Choose a number between 1 and 7");
+                    int input = int.Parse(Console.ReadLine());
+                    SetStartLocation(input);
+                    if (input < 0 && input > 7) throw new ArgumentException("Choose a number between 1 and 7");
 
                     Console.WriteLine("Select end location: ");
                     Console.WriteLine("[1] Stockholm");
@@ -47,9 +100,9 @@ namespace Lab3___Function
                     Console.WriteLine("[5] Helsinki");
                     Console.WriteLine("[6] Berlin");
                     Console.WriteLine("[7] Paris");
-                    menu = int.Parse(Console.ReadLine());
-                    SetEndLocation(menu);
-                    if (menu < 0 && menu > 7) throw new ArgumentException("Choose a number between 1 and 7");
+                    input = int.Parse(Console.ReadLine());
+                    SetEndLocation(input);
+                    if (input < 0 && input > 7) throw new ArgumentException("Choose a number between 1 and 7");
                     break;
                 }
                 catch (ArgumentException e)
@@ -62,13 +115,13 @@ namespace Lab3___Function
                     Console.Clear();
                     Console.WriteLine("Use numbers.");
                 }
+                Console.Clear();
+                Console.WriteLine("Press any button to continue. . .");
+                Console.ReadKey();
+                double distance = GetDistanceKilometers(startLatitude, endLatitude, startLongitude, endLongitude);
+                Console.WriteLine($"Distance between {startLocation} and {endLocation} is {distance}km");
+                Console.ReadKey();
             }
-            Console.Clear();
-            Console.WriteLine("Press any button to continue. . .");
-            Console.ReadKey();
-            double distance = GetDistanceKilometers(startLatitude, endLatitude, startLongitude, endLongitude);
-            Console.WriteLine($"Distance between {startLocation} and {endLocation} is {distance}km");
-            Console.ReadKey();
         }
 
         private static double GetDistanceKilometers(double xAxis1, double xAxis2, double yAxis1, double yAxis2)
